eff110e76983c898c9b5e416b9c467562614bf87
Jdure <22172148+Jdure@users.noreply.github.com>
2023-06-09 13:58:55 -0400

    CTA in contact us

--- src/app/requests/page.tsx
+++ src/app/requests/page.tsx
@@ -7,16 +7,17 @@ export default function Requests() {
         Start Your Project with a Quote!
       </h1>
       <div className="flex flex-col items-center justify-start h-screen">
-        <div className="flex flex-col items-center h-1/5 w-11/12 text-justify text-lg space-y-6">
+        <div className="flex flex-col items-center h-2/5 w-11/12 text-justify text-lg space-y-6">
           <p>
             To get started, please fill out the form below with your contact
             details and project requirements.
           </p>
-          <p>
+          <p className="pb-12">
             Our team will review your information and get back to you promptly.
           </p>
+          <div className="bg-[url('https://dummyimage.com/720x720')] bg-cover bg-center h-48 w-full rounded-lg"></div>
         </div>
-        <div className="flex flex-col items-center bg-indigo-500 h-4/5"></div>
+        <div className="flex flex-col items-center bg-indigo-500 h-3/5"></div>
       </div>
     </>
   );
